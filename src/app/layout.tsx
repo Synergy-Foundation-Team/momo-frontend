252cb653843810575a2758c6f96c1a891d4386b3
piyawat.j <piyawat.j@merudy.com>
2025-02-21 22:33:28 +0700

    updated: admin UI and fixed hydration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="th">
+    <html lang="en">
       <body
         className={`${kanit.variable} font-sans antialiased`}
         suppressHydrationWarning>
